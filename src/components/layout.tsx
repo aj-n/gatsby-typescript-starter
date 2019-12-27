80a075b2441a75ed6bceac78f7bbad1a4eae9939
AJ Newton <AJ-N@users.noreply.github.com>
2019-12-27 00:04:28 -0500

    Add primaitve site navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,47 @@
 import * as React from "react";
 
+import { graphql, Link, useStaticQuery } from "gatsby";
 import { rhythm } from "../utils/typography";
 
-const MainLayout: React.FC = ({ children }) => (
-    <div
-        style={{
-            margin: `0 auto`,
-            marginBottom: rhythm(1.5),
-            marginTop: rhythm(1.5),
-            maxWidth: 650,
-            paddingLeft: rhythm(3 / 4),
-            paddingRight: rhythm(3 / 4),
-        }}
-    >
-        {children}
-    </div>
-);
+type LayoutQueryResult = {
+    site: {
+        siteMetadata: {
+            title: string;
+        };
+    };
+};
+
+const MainLayout: React.FC = ({ children }) => {
+    const data = useStaticQuery<LayoutQueryResult>(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title: siteName
+                    }
+                }
+            }
+        `,
+    );
+
+    return (
+        <div
+            style={{
+                margin: `0 auto`,
+                marginBottom: rhythm(1.5),
+                marginTop: rhythm(1.5),
+                maxWidth: 650,
+                paddingLeft: rhythm(3 / 4),
+                paddingRight: rhythm(3 / 4),
+            }}
+        >
+            <Link to={`/`}>
+                <h3>{data.site.siteMetadata.title}</h3>
+            </Link>
+            <Link to={`/about/`}>About</Link>
+            {children}
+        </div>
+    );
+};
 
 export default MainLayout;
